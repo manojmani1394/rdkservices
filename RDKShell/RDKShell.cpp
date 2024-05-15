e3eb1fe57286acc0407310bab6ca31f1f75309df
manojmani1394 <131688704+manojmani1394@users.noreply.github.com>
2024-05-15 11:04:57 +0100

    Update RDKShell.cpp Add locking in launchWrapper

--- RDKShell/RDKShell.cpp
+++ RDKShell/RDKShell.cpp
@@ -3866,6 +3866,7 @@ namespace WPEFramework {
 
         uint32_t RDKShell::launchWrapper(const JsonObject& parameters, JsonObject& response)
         {
+	    gLaunchMutex.lock();
             LOGINFOMETHOD();
 
             double launchStartTime = RdkShell::seconds();
@@ -3930,9 +3931,9 @@ namespace WPEFramework {
                 gLaunchDestroyMutex.unlock();
                 if (isApplicationBeingDestroyed)
                 {
-                    gLaunchMutex.lock();
+                    //gLaunchMutex.lock();
                     gLaunchCount = 0;
-                    gLaunchMutex.unlock();
+                    //gLaunchMutex.unlock();
                     response["message"] = "failed to launch application due to active destroy request";
                     returnResponse(false);
                 }
@@ -4108,9 +4109,9 @@ namespace WPEFramework {
 		if (!newPluginFound && !originalPluginFound)
                 {
                     response["message"] = "failed to launch application.  type not found";
-                    gLaunchMutex.lock();
+                    //gLaunchMutex.lock();
                     gLaunchCount = 0;
-                    gLaunchMutex.unlock();
+                    //gLaunchMutex.unlock();
 		    gLaunchDestroyMutex.lock();
                     gLaunchApplications.erase(appCallsign);
 		    gLaunchDestroyMutex.unlock();
@@ -4644,9 +4645,9 @@ namespace WPEFramework {
                             break;
                     }
                     std::cout << "Application:" << callsign << " took " << (RdkShell::seconds() - launchStartTime)*1000 << " milliseconds to launch " << std::endl;
-                    gLaunchMutex.lock();
+                    //gLaunchMutex.lock();
                     gLaunchCount = 0;
-                    gLaunchMutex.unlock();
+                    //gLaunchMutex.unlock();
                     if (setSuspendResumeStateOnLaunch && deferLaunch && ((launchType == SUSPEND) || (launchType == RESUME)))
                     {
                         std::cout << "deferring application launch " << std::endl;
@@ -4663,14 +4664,14 @@ namespace WPEFramework {
             {
                 response["message"] = "failed to launch application";
             }
-            gLaunchMutex.lock();
+            //gLaunchMutex.lock();
             gLaunchCount = 0;
-            gLaunchMutex.unlock();
+           // 
 	    gLaunchDestroyMutex.lock();
             gLaunchApplications.erase(appCallsign);
 	    gLaunchDestroyMutex.unlock();
             std::cout << "new launch count at loc2 is 0\n";
-
+            gLaunchMutex.unlock();
             returnResponse(result);
         }
 
